502d29e117bf359d9d4570837b5c5085f7af6ac2
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2022-05-25 22:16:03 +0300

    repo+service

--- src/main/java/by/asalalaiko/myhome/service/UserService.java
+++ src/main/java/by/asalalaiko/myhome/service/UserService.java
@@ -2,7 +2,19 @@ package by.asalalaiko.myhome.service;
 
 import by.asalalaiko.myhome.dto.User;
 
+import java.util.List;
+
 public interface UserService {
 
+    User getUser (Long id);
+
+    User getUserByLogin(String login);
+
     boolean addUser(User user);
+
+    public void saveUser(User user);
+
+    void deleteUser(Long id);
+
+    public List<User> findAllUsers();
 }
