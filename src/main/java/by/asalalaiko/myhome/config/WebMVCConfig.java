4d37d33ff2e25cd860d08b4135ff68516b7f60f4
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2022-07-05 22:58:04 +0300

    add WebMVCConfig
    RestTemplate
    addViewControllers

---
+++ src/main/java/by/asalalaiko/myhome/config/WebMVCConfig.java
@@ -0,0 +1,27 @@
+package by.asalalaiko.myhome.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMVCConfig implements WebMvcConfigurer {
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/").setViewName("index");
+        //registry.addViewController("/error").setViewName("error");
+        registry.addViewController("/register").setViewName("register");
+    }
+
+
+}
